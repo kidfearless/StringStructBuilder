ab454026ec9858b54c2f77004e8a27df5eb22a48
KiD Fearless <kidfearless1@gmail.com>
2020-04-03 10:16:57 -0600

    Rename repo

--- StringStructBuilder/Properties/AssemblyInfo.cs
+++ StringStructBuilder/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp12")]
+[assembly: AssemblyTitle("StringStructBuilder")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp12")]
+[assembly: AssemblyProduct("StringStructBuilder")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
