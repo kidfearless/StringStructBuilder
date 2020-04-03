bfb148cb4a16a5a455e12b19a22c7903d035f253
KiD Fearless <kidfearless1@gmail.com>
2020-04-03 10:31:28 -0600

    Update Program.cs

--- StringStructBuilder/Program.cs
+++ StringStructBuilder/Program.cs
@@ -12,7 +12,7 @@ namespace StringStructBuilder
 		static void Main(string[] args)
 		{
 			int[] sizes = new int[] { 2, 4, 6, 8, 10, 12, 16, 24, 32, 64, 65, 100, 128, 200, 255, 256, 500, 512, 1000, 1024, 2000, 2048, 4000, 4096, 8000, 8192, 10000, 10240 };
-			StreamWriter file = new StreamWriter(@"C:\sm_11\include\string_struct.inc");
+			StreamWriter file = new StreamWriter(@"C:\sm_11\include\strings_struct.inc");
 			file.WriteLine("#if defined __strings_struct_included\n#endinput\n#endif\n#define __strings_struct_included\n");
 			file.WriteLine(BuildBaseString());
 			for (int i = 0; i < sizes.Length; i++)
@@ -27,17 +27,17 @@ namespace StringStructBuilder
 
 		static string BuildString(int i)
 		{
-			return $"enum struct string_{i}" +
+			return	"#define STRING_SIZE " + $"{i}" +
+					$"enum struct string_{i}" +
 					"\n{" +
-					"\n\t#define STRING_SIZE " + $"{i}" +
 					"\n\t#include \"strings_include\"" +
 					"\n}";
 		}
 		static string BuildBaseString()
 		{
-			return $"enum struct string" +
+			return	"#define STRING_SIZE 2048" +
+					$"enum struct string" +
 					"\n{" +
-					"\n\t#define STRING_SIZE 2048" +
 					"\n\t#include \"strings_include\"" +
 					"\n}";
 		}
